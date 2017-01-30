078d0dd31713891581c802a64319392bd9e09697
Matt Thompson <matt34thompson@gmail.com>
2017-01-30 22:03:41 +0000

    GET /articles now from cosmic api

---
+++ server-old.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var db = require('./db.js');
+var _ = require('underscore');
+var Cosmic = require('cosmicjs');
+
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+
+// cosmic content call: returns everything in bucket object as json 
+const bucket = { slug: 'matttea-blog-app' };
+Cosmic.getObjects({ bucket }, function(err, res) {
+  console.log(res.objects);
+});
+
+
+// POST /articles
+app.post('/articles', function(req, res) {
+    var body = _.pick(req.body, 'title', 'author', 'content', 'apiUrl');
+
+    db.article.create(body).then(function(article) {
+        res.json(article.toJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
+});
+
+// GET /articles?q=title OR ?author=author
+app.get('/articles', function(req, res) {
+    var query = req.query;
+    var where = {};
+
+    if (query.hasOwnProperty('author')) {
+		where.author = {
+			$like: '%' + query.author + '%'
+		};
+	}
+
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.title = {
+            $like: '%' + query.q + '%'
+        };
+    }
+
+    db.article.findAll({where: where}).then(function (articles) {
+        res.json(articles);
+    }, function (e) {
+        res.status(500).send();
+    });
+});
+
+db.sequelize.sync(/*{force: true}*/).then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '.');
+	});
+});
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+//TODO query backend api using query urls
+	//change api to point to cosmic.js api urls with queries based on: return all, title, author
+	//add user entry boxes and buttons to index plug-in to search for articles based on title and author
+
+	//within server.js update the api methods and queries against the object properties (title, author)
+
+
 var express = require('express');
 var app = express();
-var db = require('./db.js');
-var _ = require('underscore');
+// var db = require('./db.js');
+// var _ = require('underscore');
 var Cosmic = require('cosmicjs');
 
 var bodyParser = require('body-parser');
@@ -11,50 +18,65 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 
-// cosmic content call
-const bucket = { slug: 'matttea-blog-app' };
-Cosmic.getObjects({ bucket }, function(err, res) {
-  console.log(res.objects);
-});
+// Cosmic config
+var config = {};
+config.bucket = {
+	slug: 'matttea-blog-app',
+	read_key: 'b0ArYqMiFgGNTU2DubNVsNnySAqQc6cbMDJyiPdQbknX7oI67K',
+	write_key: 'quarKdFMTq94uNL0Wg8FetEGVScGLsMzKC9UUQG8bFoa6HiWEw'
+};
 
 
-// POST /articles
-app.post('/articles', function(req, res) {
-    var body = _.pick(req.body, 'title', 'author', 'content', 'apiUrl');
+// GET /articles
+// app.get('/articles', function(req, res) {
+// 	var query = req.query;
+// 	var where = {};
 
-    db.article.create(body).then(function(article) {
-        res.json(article.toJSON());
-    }, function(e) {
-        res.status(400).json(e);
-    });
-});
+// 	if (query.hasOwnProperty('author')) {
+// 		where.author = {
+// 			$like: '%' + query.author + '%'
+// 		};
+// 	}
 
-// GET /articles?q=title
+// 	if (query.hasOwnProperty('q') && query.q.length > 0) {
+// 		where.title = {
+// 			$like: '%' + query.q + '%'
+// 		};
+// 	}
+
+// 	db.article.findAll({where: where}).then(function (articles) {
+//         res.json(articles);
+//     }, function (e) {
+//         res.status(500).send();
+//     });
+// });
+
+
+
+// GET /articles
 app.get('/articles', function(req, res) {
-    var query = req.query;
-    var where = {};
 
-    if (query.hasOwnProperty('author')) {
-		where.author = {
-			$like: '%' + query.author + '%'
-		};
-	}
-
-    if (query.hasOwnProperty('q') && query.q.length > 0) {
-        where.title = {
-            $like: '%' + query.q + '%'
-        };
-    }
-
-    db.article.findAll({where: where}).then(function (articles) {
-        res.json(articles);
-    }, function (e) {
-        res.status(500).send();
-    });
+	var articlesParams = {
+		type_slug: 'articles',
+		limit: 500,
+		skip: 0
+	};
+	Cosmic.getObjectType(config, articlesParams, function(error, response) {
+		res.json(response);
+	}, function (e) {
+		res.status(500).send();
+	});
 });
 
-db.sequelize.sync(/*{force: true}*/).then(function() {
-	app.listen(PORT, function() {
-		console.log('Express listening on port ' + PORT + '.');
-	});
+
+// Get object
+// var objectParams = {
+// 	slug: 'part-2-the-beginnings-of-addiction'
+// }
+// Cosmic.getObject(config, objectParams, function(error, response) {
+// 	console.log(response);
+// });
+
+app.listen(PORT, function() {
+	console.log('Express listening on port ' + PORT + '.');
 });

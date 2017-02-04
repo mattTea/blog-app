d032961a501056a811b1f03beaa2ccb06d549f92
Matt Thompson <matt34thompson@gmail.com>
2017-02-04 20:20:56 +0000

    Search improvements

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.get('/articles', function(req, res) {
 
 //https://api.cosmicjs.com/v1/matttea-blog-app/object-type/articles/search?metafield_key=headline&metafield_value=Bonus blog 1
 //https://api.cosmicjs.com/v1/matttea-blog-app/object/part-1-a-taster
+//https://api.cosmicjs.com/v1/matttea-blog-app/object-type/articles/search?metafield_key=headline&metafield_value_has=taster
 
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '.');

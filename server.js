68582ec2006563646ec657c7ae61cf1eea763bc4
Matt Thompson <matt34thompson@gmail.com>
2017-02-24 21:29:13 +0000

    POST comments sorted (not display)

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var app = express();
-var comment = require('./comment.js')
-// var db = require('./db.js');
-// var _ = require('underscore');
 var Cosmic = require('cosmicjs');
 
 var bodyParser = require('body-parser');
@@ -19,26 +16,6 @@ config.bucket = {
 	read_key: 'b0ArYqMiFgGNTU2DubNVsNnySAqQc6cbMDJyiPdQbknX7oI67K',
 	write_key: 'quarKdFMTq94uNL0Wg8FetEGVScGLsMzKC9UUQG8bFoa6HiWEw'
 };
-
-
-// GET /articles
-app.get('/articles', function(req, res) {
-
-	var articlesParams = {
-		type_slug: 'articles',
-		limit: 500,
-		skip: 0
-	};
-	Cosmic.getObjectType(config, articlesParams, function(error, response) {
-		res.json(response);
-	}, function (e) {
-		res.status(500).send();
-	});
-});
-
-//POST /comments
-app.post('/comment', comment.postComment);
-
 
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '.');
